95b4c47cd581cf47155b029558a1f3769d02e10d
NdubuisiR <ndubuisirowland18565@gmail.com>
2022-07-06 06:27:01 -0700

    Updated Print Alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,22 @@
 /*
-* File: 10-add.c
-* Auth: AGBAHIME ROWLAND
+* File: 2-print_alphabet_x10.c
+* Auth: Agbahime Rowland
 */
 
 #include "main.h"
 
 /**
-* add - Adds two integers.
-* @num1: The first integer to be added.
-* @num2: The second integer to be added.
-*
-* Return: The result of the addition.
+* print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
 */
-int add(int num1, int num2)
+void print_alphabet_x10(void)
 {
-return (num1 + num2);
+int count = 0;
+char letter;
+
+while (count++ <= 9)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
+}
 }
